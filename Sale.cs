fdc8eb964985c3cf756586489cf7742b474a3bc9
ilya.luzyanin <ilya.luzyanin@check24.de>
2016-11-11 16:09:05 +0100

    Add Salesman and Sale entities

---
+++ Sale.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApplication
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+
+        public int SalesmanId { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Salesman.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApplication
+{
+    public class Salesman
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
